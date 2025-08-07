879c97bc2b0d4e7a5ae0e5983bcadc5e3d15c532
Pontsho <pontshotau097@gmail.com>
2025-08-08 01:09:38 +0200

    Initialisation

---
+++ jest.config.js
@@ -0,0 +1,30 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+    dir: './',
+})
+
+const customJestConfig = {
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+        '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+        '^@/components/(.*)$': '<rootDir>/src/components/$1',
+        '^components/(.*)$': '<rootDir>/src/components/$1',
+
+    },
+    testEnvironment: 'jsdom',
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'src/components/**/*.{js,jsx,ts,tsx}',
+        'src/pages/**/*.{js,jsx,ts,tsx}',  // optional
+        "src/app/**/*.{js,jsx}",
+        '!**/*.test.{js,jsx,ts,tsx}',      // exclude test files
+        '!**/node_modules/**',
+        '!**/.next/**',
+    ], coverageReporters: ['lcov', 'text']
+
+}
+
+module.exports = {
+    testEnvironment: "jest-environment-jsdom",
+};
--- src/app/page.js
+++ src/app/page.js
@@ -1,19 +1,11 @@
-// app/page.jsx
-import Link from 'next/link';
+import Index from './pages/index';
 
 export default function HomePage() {
   return (
-    <main className="p-8 text-center">
-      <h1 className="text-3xl font-bold mb-6">Welcome to My Site</h1>
-
-      <div className="flex justify-center gap-6">
-        <Link href="/pages/about" className="text-blue-600 underline">
-          Go to About
-        </Link>
-        <Link href="/pages/contact" className="text-blue-600 underline">
-          Go to Contact&apos;s
-        </Link>
-      </div>
-    </main>
+    <>
+      <main>
+        <Index/>
+      </main>
+    </>
   );
 }
