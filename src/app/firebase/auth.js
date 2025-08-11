89809e86cae63397d9c0f2fc0d042c9578045902
Ponani24 <ponngobeni24@gmail.com>
2025-08-11 04:55:19 +0200

    UI for google signup

--- src/app/firebase/auth.js
+++ src/app/firebase/auth.js
@@ -1,6 +1,60 @@
-import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, sendPasswordResetEmail } from "firebase/auth";
 import app from "./config";
 
 const auth = getAuth(app);
+export async function signUp(email, password) {
+  try {
+    // Create a new user with email and password
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    // Send verification email
+    await sendEmailVerification(userCredential.user);
+    return userCredential.user; // Return the newly created user
+  } catch (error) {
+    throw error; // Re-throw the error for handling in the component
+  }
+}
+
+export async function signUpWithGoogle() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    return result.user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function signIn(email, password) {
+  const auth = getAuth();
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+// Sign in with Google
+export async function signInWithGoogle() {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  try {
+    const result = await signInWithPopup(auth, provider);
+    return result.user;
+  } catch (error) {
+    throw error;
+  }
+}
+
+export async function forgotPassword(email) {
+  const auth = getAuth();
+  try {
+    await sendPasswordResetEmail(auth, email);
+    return true;
+  } catch (error) {
+    throw error;
+  }
+}
 
 export { auth };
