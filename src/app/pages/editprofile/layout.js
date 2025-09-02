47a7fb724c2ddc6eff543330944b884d429f6c25
MakomaneTau <151774523+MakomaneTau@users.noreply.github.com>
2025-09-02 15:30:43 +0200

    Update

--- src/app/pages/editprofile/layout.js
+++
@@ -1,8 +0,0 @@
-import ProtectedLayout from "@/app/components/ProtectedLayout";
-export default function DashboardLayout({ children }) {
-  return (
-    <ProtectedLayout redirectTo="/">
-      {children}
-    </ProtectedLayout>
-  );
-}
